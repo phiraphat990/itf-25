02853b3ec0bfe449e76b8316628cb0508868bb0c
phiraphat990 <73680333+phiraphat990@users.noreply.github.com>
2020-10-30 10:53:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php
-echo "<h2>ชื่อ นายพีรพัฒน์ กิจพร้อมผล ชื่อเล่น สุนทร</h2>";
-echo "<br>";
-?>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My First Paragraph</p>
+        <img src="cat.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </u>
+    </body>
+</html>
